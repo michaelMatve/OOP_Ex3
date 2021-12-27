eafdd4ad055af5ab63b06c90d63c046efe6aa7f9
EylonNaamat <eylonnaamat@gmail.com>
2021-12-27 20:36:32 +0200

    documentation

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -17,6 +17,8 @@ class GraphAlgo(GraphAlgoInterface):
             graph = DiGraph()
         self.graph = graph
 
+
+
     def get_graph(self) -> GraphInterface:
         return self.graph
 
@@ -90,41 +92,53 @@ class GraphAlgo(GraphAlgoInterface):
         return (answerlist, sum)
 
 
+    """
+    this function calculates the center of the graph, 
+    it calculates the max_dist of every node and returns the id of the center along with distance
+    """
+
     def centerPoint(self) -> (int, float):
-        if not self.is_connected():
+        if not self.is_connected():  # cheack if the graph is connected, if not it means no center
             return (-1 , float("inf"))
-        min = float("inf")
+        min = float("inf")  # sets the min to float("inf"), and the id to None
         min_id = None
-        for node in self.graph.nodes.values():
+        for node in self.graph.nodes.values():  # go through every node in the graph, and checks if it max_dist is the minimum
             temp = self.max_dist(node.id)
-            if temp<=min:
+            if temp<=min:  # if so updated the id of the center, and the distance
                 min = temp
                 min_id = node.id
         return (min_id, min)
 
 
+    """
+    this function draws the line on the screen
+    """
 
     def plot_graph(self) -> None:
-        for node in self.graph.nodes.values():
-            if node.pos == None:
+        for node in self.graph.nodes.values(): # go through every node in the graph
+            if node.pos == None:  # if the node doesnt have a pos, creates a new pos for him
                 node.pos = self.random_pos()
-        for node in self.graph.nodes.values():
+        for node in self.graph.nodes.values():  # go through every node in the graph
             x, y, z = node.pos
-            plt.plot(x, y, markersize= 10, marker= "o", color= "black")
+            plt.plot(x, y, markersize= 10, marker= "o", color= "black")  # uses plot and text functions to draw the node on the screen
             plt.text(x, y, str(node.id), color= "red", fontsize= 12)
-            for neigh in node.out_edges:
+            for neigh in node.out_edges:  # go through every in edge the node has
                 neigh_x, neigh_y, neigh_z = self.graph.nodes.get(neigh).pos
-                plt.annotate("", xy=(x, y), xytext=(neigh_x, neigh_y), arrowprops=dict(arrowstyle="<|-"))
+                plt.annotate("", xy=(x, y), xytext=(neigh_x, neigh_y), arrowprops=dict(arrowstyle="<|-"))  # draws an edge to that node
 
         plt.show()
 
 
+    """
+    this function generates a random pos fo the node
+    """
+
     def random_pos(self) -> tuple:
-        x = random.uniform(35.187594216303474, 35.21310882485876)
-        y = random.uniform(32.10152879327731, 32.10788938151261)
+        x = random.uniform(35.187594216303474, 35.21310882485876)  # creates random x, in range
+        y = random.uniform(32.10152879327731, 32.10788938151261)  # creates random y, in range
         z = 0.0
         ans = (x, y, z)
-        return ans
+        return ans  # returns the new pos
 
 
 
@@ -162,34 +176,50 @@ class GraphAlgo(GraphAlgoInterface):
                         run_node = node
         return
 
+
+    """
+    this returns the max shortest path from this node to a certain node in the graph
+    """
+
     def max_dist(self, src):
-        self.dijkstra_algo(src)
+        self.dijkstra_algo(src)  # makes dijkstra on this node to calculate shortest path from this node to every node in the graph
         max =0.0
-        for node in self.graph.nodes.values():
+        for node in self.graph.nodes.values():  # go through every node in the graph, and if there is a path longer than max sets max to it
             if node.weight>max and node.weight!= float("inf"):
                 max = node.weight
         return max
 
+
+    """
+    this function makes dfs from a certain node
+    """
+
     def dfs(self, node : Node) -> None:
-        stack = list()
-        stack.append(node)
-        while stack:
+        stack = list() # create a stack
+        stack.append(node) # push the node to the stack
+        while stack: # as long as the stack is not empty, pop the stack
             node = stack.pop()
-            if node.tag == -1:
+            if node.tag == -1: # checks if the node is not visited, if not change it to visited
                 node.tag = 1
 
-            for i in node.out_edges:
-                if self.graph.nodes.get(i).tag == -1:
+            for i in node.out_edges: # go through the nodes neighbours
+                if self.graph.nodes.get(i).tag == -1: # if the neighbour is not visited yet, push him to the stack
                     stack.append(self.graph.nodes.get(i))
 
+    """
+    this function makes a transpose graph out of the original graph
+    """
 
     def get_transpose(self):
-        for i in self.graph.nodes.values():
+        for i in self.graph.nodes.values(): # go through all the nodes and changes the in_edges to out_edges and vice versa
             temp = i.in_edges.copy()
             i.in_edges = i.out_edges.copy()
             i.out_edges = temp.copy()
 
 
+    """
+    this function sets all the nodes tag to -1, the weight to float("inf"), the info to "w" (not visited)
+    """
 
     def set_tags_weight(self) -> None:
         for i in self.graph.nodes.values():
@@ -197,21 +227,25 @@ class GraphAlgo(GraphAlgoInterface):
             i.weight = float("inf")
             i.info = 'w'
 
+    """
+    this function checks if the graph is SCC, if so, it returns true, else false
+    """
+
     def is_connected(self):
-        self.set_tags_weight()
-        self.dfs(self.graph.nodes.get(0))
-        for i in self.graph.nodes.values():
+        self.set_tags_weight() # sets the tag of every node to -1
+        self.dfs(self.graph.nodes.get(0)) # does dfs on the first node in the graph
+        for i in self.graph.nodes.values(): # checks if after the dfs there is a node with tag -1, if so it means the graph isnt SCC
             if i.tag == -1:
                 return False
 
-        self.get_transpose()
-        self.set_tags_weight()
-        self.dfs(self.graph.nodes.get(0))
-        for i in self.graph.nodes.values():
+        self.get_transpose() # make a transpose graph
+        self.set_tags_weight() # sets the tag of every node to -1
+        self.dfs(self.graph.nodes.get(0)) # does dfs on the first node in the graph
+        for i in self.graph.nodes.values(): # checks if after the dfs there is a node with tag -1, if so it means the graph isnt SCC
             if i.tag == -1:
-                self.get_transpose()
+                self.get_transpose() # returns the graph to it original form
                 return False
-        self.get_transpose()
+        self.get_transpose() # returns the graph to it original form
         return True
 
 
