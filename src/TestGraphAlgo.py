0bf8aff09e516c9625beab64727b887da08657fc
EylonNaamat <eylonnaamat@gmail.com>
2021-12-27 20:55:46 +0200

    tests

---
+++ src/TestGraphAlgo.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+
+
+class TestGraphAlgo(TestCase):
+    def test_get_graph(self):
+        self.fail()
+
+    def test_load_from_json(self):
+        self.fail()
+
+    def test_save_to_json(self):
+        self.fail()
+
+    def test_shortest_path(self):
+        self.fail()
+
+    def test_tsp(self):
+        self.fail()
+
+    def test_center_point(self):
+        self.fail()
+
+    def test_plot_graph(self):
+        self.fail()
+
+    def test_random_pos(self):
+        self.fail()
+
+    def test_to_json_file(self):
+        self.fail()
+
+    def test_dijkstra_algo(self):
+        self.fail()
+
+    def test_max_dist(self):
+        self.fail()
+
+    def test_dfs(self):
+        self.fail()
+
+    def test_get_transpose(self):
+        self.fail()
+
+    def test_set_tags_weight(self):
+        self.fail()
+
+    def test_is_connected(self):
+        self.fail()
